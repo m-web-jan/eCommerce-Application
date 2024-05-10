b60cdbaeb743f963ef508cf94392c0b6d66a2cc4
Mark <m.web.jan@gmail.com>
2024-05-10 20:49:12 +0300

    fix: builds

--- eCommerce/vite.config.ts
+++ eCommerce/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/main.tsx"),
+        main: resolve(__dirname, "index.html"),
       },
     },
   },
