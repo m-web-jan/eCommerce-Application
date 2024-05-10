579809cf02921ee5c32817738652870da4a822e9
Mark <m.web.jan@gmail.com>
2024-05-10 20:56:23 +0300

    fix: vite.config

--- eCommerce/vite.config.ts
+++ eCommerce/vite.config.ts
@@ -1,9 +1,8 @@
 // <reference types="vitest" />
 // <reference types="vite/client" />
 
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,8 +15,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: 'index.html',
       },
     },
   },
-})
+});
