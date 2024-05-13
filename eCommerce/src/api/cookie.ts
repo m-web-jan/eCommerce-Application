054f04a9534f0119b158b2443532ee0688ef98ad
Mark <m.web.jan@gmail.com>
2024-05-13 23:32:06 +0300

    fix: change get and set cookie functions

--- eCommerce/src/api/cookie.ts
+++ eCommerce/src/api/cookie.ts
@@ -1,21 +1,11 @@
-export function setCookie(name: string, value: string, seconds: number): void {
-  const date = new Date();
-  date.setTime(date.getTime() + (seconds * 1000));
-  const expires = "expires=" + date.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
+export const setCookie = (name: string, token: string, expiresIn: number) => {
+  const deadTokenDate = new Date(Date.now() + expiresIn * 1000).toUTCString();
+  document.cookie = `${name}=${token}; expires=${deadTokenDate}`;
+};
 
-export function getCookie(name: string): string | null {
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookies = decodedCookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-      let cookie = cookies[i];
-      while (cookie.charAt(0) === ' ') {
-          cookie = cookie.substring(1);
-      }
-      if (cookie.indexOf(name + "=") === 0) {
-          return cookie.substring(name.length + 1, cookie.length);
-      }
-  }
-  return null;
+export function getCookie(name: string) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
