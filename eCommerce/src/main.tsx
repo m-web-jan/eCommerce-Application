bdfafb5f4fe10d82055f43629a8b1a54547deba1
Mark <m.web.jan@gmail.com>
2024-05-14 01:00:26 +0300

    feat: get token when loading page

--- eCommerce/src/main.tsx
+++ eCommerce/src/main.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { getAuthToken } from './api/authToken.ts';
+import { getCookie } from './api/cookie.ts';
 // import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/* <Provider store={store}> */}
+const RootComponent = () => {
+  useEffect(() => {
+    getAuthToken();
+    const mailToken = getCookie('emailToken');
+    console.log(mailToken);
+  }, []);
+
+  return (
+    <React.StrictMode>
+      {/* <Provider store={store}> */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    {/* </Provider> */}
-  </React.StrictMode>
+      {/* </Provider> */}
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RootComponent />
 );
