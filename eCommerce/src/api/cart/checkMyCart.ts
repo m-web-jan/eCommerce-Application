4579683e4523aeec7dcd329afc33f1b8fc2e1740
Mark <m.web.jan@gmail.com>
2024-06-18 12:19:47 +0300

    fix: get anonymous token

--- eCommerce/src/api/cart/checkMyCart.ts
+++ eCommerce/src/api/cart/checkMyCart.ts
@@ -1,9 +1,10 @@
 import { publicInstance } from '..';
+import { getAnonymousToken } from '../anonymousToken';
 import { getCookie } from '../cookie';
 
 export const checkExistMyCarts = async () => {
   let token = getCookie('emailToken');
-  if(!token) token = getCookie('anonymousToken');
+  if(!token) token = await getAnonymousToken();
   const { data } = await publicInstance.get(`/me/carts`, {
     headers: {
       Authorization: `Bearer ${token}`,
